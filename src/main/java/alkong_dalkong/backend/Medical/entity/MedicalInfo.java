7e3ffd22049e0770121a0fd2de53145101c87fc7
cjh-19 <gns8812@naver.com>
2024-08-21 21:09:58 +0900

    feat: List<String>을 위해 Converter 클래스 생성

--- src/main/java/alkong_dalkong/backend/Medical/entity/MedicalInfo.java
+++ src/main/java/alkong_dalkong/backend/Medical/entity/MedicalInfo.java
@@ -1,23 +1,42 @@
 package alkong_dalkong.backend.Medical.entity;
 
+import alkong_dalkong.backend.Medical.common.StringListConverter;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Getter
+@Table(name = "medicalInfo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MedicalInfo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "medical_id")
     private Long medicalId;
 
+    @Column(name = "hospital_name")
     private String hospitalName;
+
+    @Column(name = "hospital_date")
     private LocalDateTime hospitalDate;
 
-    @ElementCollection
+    // list로 저장
+    @Convert(converter = StringListConverter.class)
+    @Column(name = "madical_part")
     private List<String> medicalPart;
 
+    @Column(name = "medical_memo")
     private String medicalMemo;
+
+    @Column(name = "medical_alarm")
     private LocalDateTime medicalAlarm;
 }
