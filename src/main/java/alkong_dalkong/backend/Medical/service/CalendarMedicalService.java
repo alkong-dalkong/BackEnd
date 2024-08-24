8b65e49c44778a90dec44a594b41cadfe27db6f9
cjh-19 <gns8812@naver.com>
2024-08-24 22:00:46 +0900

    fix: repository명 변경

--- src/main/java/alkong_dalkong/backend/Medical/service/CalendarMedicalService.java
+++ src/main/java/alkong_dalkong/backend/Medical/service/CalendarMedicalService.java
@@ -2,7 +2,7 @@ package alkong_dalkong.backend.Medical.service;
 
 import alkong_dalkong.backend.Medical.dto.response.CalendarMedicalResponseDto;
 import alkong_dalkong.backend.Medical.entity.MedicalInfo;
-import alkong_dalkong.backend.Medical.repository.MedicalInfoCalendarRepository;
+import alkong_dalkong.backend.Medical.repository.MedicalInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 public class CalendarMedicalService {
 
     @Autowired
-    private MedicalInfoCalendarRepository medicalInfoCalendarRepository;
+    private MedicalInfoRepository medicalInfoRepository;
 
     public List<CalendarMedicalResponseDto> getMedicalCalendar(Long userId, String localDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
@@ -24,7 +24,7 @@ public class CalendarMedicalService {
         LocalDateTime startDate = yearMonth.atDay(1).atStartOfDay();
         LocalDateTime endDate = yearMonth.atEndOfMonth().atTime(23, 59, 59);
 
-        List<MedicalInfo> medicalInfos = medicalInfoCalendarRepository.findByUserUserIdAndHospitalDateBetween(userId, startDate, endDate);
+        List<MedicalInfo> medicalInfos = medicalInfoRepository.findByUserUserIdAndHospitalDateBetween(userId, startDate, endDate);
 
         return medicalInfos.stream().map(medicalInfo -> new CalendarMedicalResponseDto(
                 medicalInfo.getMedicalId(),
