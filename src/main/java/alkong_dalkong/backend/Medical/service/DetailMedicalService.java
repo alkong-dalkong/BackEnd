6289662b4ec6dfed85cd98aef64be46b2454bcda
cjh-19 <gns8812@naver.com>
2024-08-24 22:04:23 +0900

    feat: medical_alarm 계산 로직 메서드

--- src/main/java/alkong_dalkong/backend/Medical/service/DetailMedicalService.java
+++ src/main/java/alkong_dalkong/backend/Medical/service/DetailMedicalService.java
@@ -4,10 +4,13 @@ import alkong_dalkong.backend.Medical.dto.response.DetailMedicalResponseDto;
 import alkong_dalkong.backend.Medical.entity.MedicalInfo;
 import alkong_dalkong.backend.Medical.repository.MedicalInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Service
 public class DetailMedicalService {
 
     @Autowired
@@ -36,5 +39,24 @@ public class DetailMedicalService {
     }
 
     private int calculateAlarmIndex(LocalDateTime hospitalDate, LocalDateTime medicalAlarm) {
+        if (medicalAlarm == null) {
+            return 5;
+        }
+
+        long minutesDiff = Duration.between(medicalAlarm, hospitalDate).toMinutes();
+
+        if(minutesDiff == Duration.ofDays(7).toMinutes()) {
+            return 0;
+        } else if (minutesDiff == Duration.ofHours(24).toMinutes()) {
+            return 1;
+        } else if (minutesDiff == Duration.ofHours(12).toMinutes()) {
+            return 2;
+        } else if (minutesDiff == Duration.ofHours(1).toMinutes()) {
+            return 3;
+        } else if (minutesDiff == Duration.ofMinutes(30).toMinutes()) {
+            return 4;
+        }
+
+        return 5; // 모두 해당하지 않는 경우 -> 추후 예외처리 필요
     }
 }
