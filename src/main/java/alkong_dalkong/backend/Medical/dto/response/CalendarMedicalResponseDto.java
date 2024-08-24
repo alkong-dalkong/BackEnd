5a1d12c924c2c7e4e2fe8ad7069b79ea45b29c46
cjh-19 <gns8812@naver.com>
2024-08-24 21:11:17 +0900

    feat: Data 어노테이션 적용

--- src/main/java/alkong_dalkong/backend/Medical/dto/response/CalendarMedicalResponseDto.java
+++ src/main/java/alkong_dalkong/backend/Medical/dto/response/CalendarMedicalResponseDto.java
@@ -1,24 +1,19 @@
 package alkong_dalkong.backend.Medical.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
 public class CalendarMedicalResponseDto {
 
     private Long medicalId;
     private String hospitalName;
     private LocalDateTime hospitalDate;
     private List<String> medicalPart;
-
-    public CalendarMedicalResponseDto(Long medicalId, String hospitalName, LocalDateTime hospitalDate, List<String> medicalPart) {
-        this.medicalId = medicalId;
-        this.hospitalName = hospitalName;
-        this.hospitalDate = hospitalDate;
-        this.medicalPart = medicalPart;
-    }
 }
