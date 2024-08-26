3eebfc2d79807df40052aef61df5605bf9799b9f
cjh-19 <gns8812@naver.com>
2024-08-26 16:36:40 +0900

    feat: requestDto 생성(#42)

---
+++ src/main/java/alkong_dalkong/backend/Medical/dto/request/MedicalUpdateRequestDto.java
@@ -0,0 +1,26 @@
+package alkong_dalkong.backend.Medical.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MedicalUpdateRequestDto {
+
+    private String hospitalName;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime hospitalDate;
+
+    private List<String> medicalPart;
+    private String medicalMemo;
+    private Integer medicalAlarm;
+}
