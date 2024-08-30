dc90503615a6f654a945876a34e89caf45b1f491
pyungjong <sonopj@gmail.com>
2024-08-30 11:38:53 +0900

    MedicineController Response 수정(code 추가)

--- src/main/java/alkong_dalkong/backend/Medicine/Controller/MedicineDateController.java
+++ src/main/java/alkong_dalkong/backend/Medicine/Controller/MedicineDateController.java
@@ -3,18 +3,20 @@ package alkong_dalkong.backend.Medicine.Controller;
 import alkong_dalkong.backend.Medicine.DTO.*;
 import alkong_dalkong.backend.Medicine.DTO.Request.MedicineUpdateTakenRequest;
 import alkong_dalkong.backend.Medicine.DTO.Response.MedicineDateResponse;
-import alkong_dalkong.backend.Medicine.DTO.Response.MedicineUpdateTakenResponse;
 import alkong_dalkong.backend.Medicine.Domain.MedicineRecord;
 import alkong_dalkong.backend.Medicine.Domain.MedicineRelation;
 import alkong_dalkong.backend.Medicine.Service.MedicineRecordService;
 import alkong_dalkong.backend.Medicine.Service.MedicineRelationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -24,61 +26,70 @@ public class MedicineDateController {
     private final MedicineRecordService medicineRecordService;
 
     @GetMapping("/medicine/{medicine_user_id}/{local_date}/taken_info")
-    public MedicineDateResponse MedicineDate(@PathVariable("medicine_user_id") Long user_id,
-                                             @PathVariable("local_date")LocalDate date){
-        MedicineDateResponse medicineDateResponse = new MedicineDateResponse();
-        // 사용자가 복용하는 모든 약
-        List<MedicineRelation> medicineRelationList = medicineRelationService.FindAllUserMedicine(user_id);
+    public ResponseEntity<?> MedicineDate(@PathVariable("medicine_user_id") Long user_id,
+                                          @PathVariable("local_date")LocalDate date){
+        try {
+            MedicineDateResponse medicineDateResponse = new MedicineDateResponse();
+            // 사용자가 복용하는 모든 약
+            List<MedicineRelation> medicineRelationList = medicineRelationService.FindAllUserMedicine(user_id);
 
-        for(MedicineRelation medicineRelation : medicineRelationList){
-            // 무제한 기간 약의 복용 정보가 없는 경우 (1달 기록 생성)
-            if(medicineRelation.getTakenEndDate().equals(LocalDate.of(9999, 12, 31))){
-                medicineRelationService.addMedicineInfinite(medicineRelation, date);
-            }
+            for (MedicineRelation medicineRelation : medicineRelationList) {
+                // 무제한 기간 약의 복용 정보가 없는 경우 (1달 기록 생성)
+                if (medicineRelation.getTakenEndDate().equals(LocalDate.of(9999, 12, 31))) {
+                    medicineRelationService.addMedicineInfinite(medicineRelation, date);
+                }
 
-            // 특정 날짜에 복용하는 약
-            if(medicineRecordService.FindAllDateByMedicine(medicineRelation.getId()).contains(date)){
-                MedicineDateDto medicineDateDto = new MedicineDateDto();
-                medicineDateDto.setMedicineId(medicineRelation.getMedicine().getId());
-                medicineDateDto.setMedicineName(medicineRelation.getMedicine().getMedicineName());
-                medicineDateDto.setMedicineDosage(medicineRelation.getDosage());
-                medicineDateDto.setMedicineTakenType(medicineRelation.getMedicineTakenType());
+                // 특정 날짜에 복용하는 약
+                if (medicineRecordService.FindAllDateByMedicine(medicineRelation.getId()).contains(date)) {
+                    MedicineDateDto medicineDateDto = new MedicineDateDto();
+                    medicineDateDto.setMedicineId(medicineRelation.getMedicine().getId());
+                    medicineDateDto.setMedicineName(medicineRelation.getMedicine().getMedicineName());
+                    medicineDateDto.setMedicineDosage(medicineRelation.getDosage());
+                    medicineDateDto.setMedicineTakenType(medicineRelation.getMedicineTakenType());
 
-                // 약 복용 정보
-                List<LocalTime> timeList = medicineRelation.takenTime();
-                MedicineRecord medicineRecord = medicineRecordService.dateMedicineRecord(date, medicineRelation.getId());
-                medicineDateDto.setMedicineRecordId(medicineRecord.getId());
+                    // 약 복용 정보
+                    List<LocalTime> timeList = medicineRelation.takenTime();
+                    MedicineRecord medicineRecord = medicineRecordService.dateMedicineRecord(date, medicineRelation.getId());
+                    medicineDateDto.setMedicineRecordId(medicineRecord.getId());
 
-                for(int i = 0; i < timeList.size(); i++){
-                    MedicineDateTakenInfo medicineDateTakenInfo = new MedicineDateTakenInfo();
-                    medicineDateTakenInfo.setMedicine_id(medicineRelation.getMedicine().getId());
-                    medicineDateTakenInfo.setIndex(i);
-                    switch(i){
-                        case 0 -> {
-                            medicineDateTakenInfo.setTaken(medicineRecord.getBreakfast());
-                            medicineDateResponse.addMedicineTakenInfo(medicineRelation.getMedicineBreakfast(), medicineDateTakenInfo);
-                        }
-                        case 1 -> {
-                            medicineDateTakenInfo.setTaken(medicineRecord.getLunch());
-                            medicineDateResponse.addMedicineTakenInfo(medicineRelation.getMedicineLunch(), medicineDateTakenInfo);
-                        }
-                        case 2 -> {
-                            medicineDateTakenInfo.setTaken(medicineRecord.getDinner());
-                            medicineDateResponse.addMedicineTakenInfo(medicineRelation.getMedicineDinner(), medicineDateTakenInfo);
+                    for (int i = 0; i < timeList.size(); i++) {
+                        MedicineDateTakenInfo medicineDateTakenInfo = new MedicineDateTakenInfo();
+                        medicineDateTakenInfo.setMedicine_id(medicineRelation.getMedicine().getId());
+                        medicineDateTakenInfo.setIndex(i);
+                        switch (i) {
+                            case 0 -> {
+                                medicineDateTakenInfo.setTaken(medicineRecord.getBreakfast());
+                                medicineDateResponse.addMedicineTakenInfo(medicineRelation.getMedicineBreakfast(), medicineDateTakenInfo);
+                            }
+                            case 1 -> {
+                                medicineDateTakenInfo.setTaken(medicineRecord.getLunch());
+                                medicineDateResponse.addMedicineTakenInfo(medicineRelation.getMedicineLunch(), medicineDateTakenInfo);
+                            }
+                            case 2 -> {
+                                medicineDateTakenInfo.setTaken(medicineRecord.getDinner());
+                                medicineDateResponse.addMedicineTakenInfo(medicineRelation.getMedicineDinner(), medicineDateTakenInfo);
+                            }
                         }
                     }
+                    medicineDateResponse.getMedicineDateDtoList().add(medicineDateDto);
                 }
-                medicineDateResponse.getMedicineDateDtoList().add(medicineDateDto);
             }
+
+            return ResponseEntity.ok().body(Map.of("code", 200, "data", medicineDateResponse));
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("code", 500, "error", "예상치 못한 오류 발생"));
         }
-        return medicineDateResponse;
     }
 
     @PatchMapping("/medicine/{medicine_record_id}/taken")
-    public MedicineUpdateTakenResponse MedicineUpdateTaken(@PathVariable("medicine_record_id") Long medicine_record_id,
-                                                           @RequestBody @Valid MedicineUpdateTakenRequest request){
-        medicineRecordService.updateMedicineTaken(medicine_record_id, request.getTimeNum(), request.getTakenNum());
+    public ResponseEntity<?> MedicineUpdateTaken(@PathVariable("medicine_record_id") Long medicine_record_id,
+                                                           @RequestBody @Valid MedicineUpdateTakenRequest request) {
+        try {
+            medicineRecordService.updateMedicineTaken(medicine_record_id, request.getTimeNum(), request.getTakenNum());
 
-        return new MedicineUpdateTakenResponse(medicine_record_id);
+            return ResponseEntity.ok().body(Map.of("code", 200));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("code", 500, "error", "예상치 못한 오류 발생"));
+        }
     }
 }
--- src/main/java/alkong_dalkong/backend/Medicine/Controller/UserMedicineController.java
+++ src/main/java/alkong_dalkong/backend/Medicine/Controller/UserMedicineController.java
@@ -4,6 +4,8 @@ import alkong_dalkong.backend.Medicine.DTO.Response.UserTotalMedicineResponse;
 import alkong_dalkong.backend.Medicine.Domain.MedicineRelation;
 import alkong_dalkong.backend.Medicine.Service.MedicineRelationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,34 +23,41 @@ public class UserMedicineController {
     private final MedicineRelationService medicineRelationService;
 
     @GetMapping("/medicine/{medicine_user_id}/total_medicine_info")
-    public List<UserTotalMedicineResponse> UserTotalMedicine(@PathVariable("medicine_user_id") Long user_id){
-        List<MedicineRelation> medicineRelationList = medicineRelationService.FindAllUserMedicine(user_id);
+    public ResponseEntity<?> UserTotalMedicine(@PathVariable("medicine_user_id") Long user_id){
+        try {
+            List<MedicineRelation> medicineRelationList = medicineRelationService.FindAllUserMedicine(user_id);
+            // UserTotalMedicineResponse 생성
+            List<UserTotalMedicineResponse> resultList = new ArrayList<>();
+            for (MedicineRelation medicineRelation : medicineRelationList) {
+                // 약을 복용하는 요일 리스트
+                List<DayOfWeek> weekList = medicineRelation.possibleWeek();
+                // 약을 복용하는 시간 리스트
+                List<LocalTime> timeList = medicineRelation.takenTime();
 
-        // UserTotalMedicineResponse 생성
-        List<UserTotalMedicineResponse> resultList = new ArrayList<>();
-        for(MedicineRelation medicineRelation : medicineRelationList){
-            // 약을 복용하는 요일 리스트
-            List<DayOfWeek> weekList = medicineRelation.possibleWeek();
-            // 약을 복용하는 시간 리스트
-            List<LocalTime> timeList = medicineRelation.takenTime();
+                UserTotalMedicineResponse response =
+                        new UserTotalMedicineResponse(medicineRelation.getMedicine().getId(),
+                                medicineRelation.getMedicine().getMedicineName(), weekList, timeList,
+                                medicineRelation.getDosage(), medicineRelation.getMedicineTakenType()
+                                , medicineRelation.getMedicineMemo());
+                resultList.add(response);
+            }
 
-            UserTotalMedicineResponse response =
-                    new UserTotalMedicineResponse(medicineRelation.getMedicine().getId(),
-                            medicineRelation.getMedicine().getMedicineName(), weekList, timeList,
-                            medicineRelation.getDosage(), medicineRelation.getMedicineTakenType()
-                            ,medicineRelation.getMedicineMemo());
-            resultList.add(response);
+            return ResponseEntity.ok().body(Map.of("code", 200, "data", resultList));
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("code", 500, "error", "예상치 못한 오류 발생"));
         }
-
-        return resultList;
     }
 
 
     @DeleteMapping("/medicine/{medicine_user_id}/{medicine_id}/delete")
-    public int MedicineDelete(@PathVariable("medicine_user_id") Long user_id,
-                              @PathVariable("medicine_id") Long medicine_id){
+    public ResponseEntity<?> MedicineDelete(@PathVariable("medicine_user_id") Long user_id,
+                              @PathVariable("medicine_id") Long medicine_id) {
+        try {
+            medicineRelationService.removeMedicineRelation(user_id, medicine_id);
 
-        medicineRelationService.removeMedicineRelation(user_id, medicine_id);
-        return 0;
+            return ResponseEntity.ok().body(Map.of("code", 200));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("code", 500, "error", "예상치 못한 오류 발생"));
+        }
     }
 }
