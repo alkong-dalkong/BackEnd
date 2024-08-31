efd4b3bf62d0e4aacf7ac0ffeb2665de6d78b343
cjh-19 <gns8812@naver.com>
2024-08-31 18:23:40 +0900

    feat: 각 기능에 맞는 정보를 조회하는 jpa 추가(#54)

--- src/main/java/alkong_dalkong/backend/Medical/repository/MedicalInfoRepository.java
+++ src/main/java/alkong_dalkong/backend/Medical/repository/MedicalInfoRepository.java
@@ -5,7 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MedicalInfoRepository extends JpaRepository<MedicalInfo, Long> {
+
+    // 주어진 시간 사이의 병원 내원 정보
     List<MedicalInfo> findByUserUserIdAndHospitalDateBetween(Long userId, LocalDateTime startDate, LocalDateTime endDate);
+
+    // 최근 병원 내원 일정
+    Optional<MedicalInfo> findTopByUserUserIdAndHospitalDateLessThanEqualOrderByHospitalDateDesc(Long userId, LocalDateTime localDate);
+
+    // 다가오는 병원 내원 일정
+    Optional<MedicalInfo> findTopByUserUserIdAndHospitalDateGreaterThanEqualOrderByHospitalDateAsc(Long userId, LocalDateTime localDate);
 }
