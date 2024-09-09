bb5a73e3d8654d476fe5bd3851be406d22c82ef8
cjh-19 <gns8812@naver.com>
2024-09-09 22:45:19 +0900

    feat: post service 로직(#68)

---
+++ src/main/java/alkong_dalkong/backend/Physical/service/WeightService.java
@@ -0,0 +1,38 @@
+package alkong_dalkong.backend.Physical.service;
+
+import alkong_dalkong.backend.Physical.dto.request.PhysicalRequestDto;
+import alkong_dalkong.backend.Physical.entity.PhysicalInfo;
+import alkong_dalkong.backend.Physical.entity.WeightInfo;
+import alkong_dalkong.backend.Physical.repository.PhysicalInfoRepository;
+import alkong_dalkong.backend.Physical.repository.WeightInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class WeightService {
+
+    @Autowired
+    private PhysicalInfoRepository physicalInfoRepository;
+
+    @Autowired
+    private WeightInfoRepository weightInfoRepository;
+
+    // 체중 정보 추가
+    public Long addWeight(PhysicalRequestDto requestDto) {
+        Optional<PhysicalInfo> physicalInfoOpt = physicalInfoRepository.findById(requestDto.getPhysicalId());
+        if (physicalInfoOpt.isEmpty()) {
+            throw new IllegalArgumentException("해당 유저의 건강 정보가 존재하지 않습니다: " + requestDto.getPhysicalId());
+        }
+
+        PhysicalInfo physicalInfo = physicalInfoOpt.get();
+
+        WeightInfo weightInfo = new WeightInfo();
+        weightInfo.setWeight(requestDto.getWeight());
+        weightInfo.setCreatedAt(requestDto.getCreatedAt());
+        weightInfo.setPhysicalInfo(physicalInfo);
+
+        return weightInfoRepository.save(weightInfo).getWeightId();
+    }
+}
