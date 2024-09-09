ca459ca9f1d26b6aedd440484c71ade32a0a5d27
cjh-19 <gns8812@naver.com>
2024-09-09 22:18:20 +0900

    feat: jpa repository 생성(#66)

---
+++ src/main/java/alkong_dalkong/backend/Physical/repository/WeightInfoRepository.java
@@ -0,0 +1,7 @@
+package alkong_dalkong.backend.Physical.repository;
+
+import alkong_dalkong.backend.Physical.entity.WeightInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeightInfoRepository  extends JpaRepository<WeightInfo, Long> {
+}
