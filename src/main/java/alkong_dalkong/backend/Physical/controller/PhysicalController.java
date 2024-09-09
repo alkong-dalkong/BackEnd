fd98dfb073f62e532b02d945e14e2a1568d057ee
cjh-19 <gns8812@naver.com>
2024-09-09 22:46:21 +0900

    feat: post restapi 매핑 및 예외처리(#68)

--- src/main/java/alkong_dalkong/backend/Physical/controller/PhysicalController.java
+++ src/main/java/alkong_dalkong/backend/Physical/controller/PhysicalController.java
@@ -1,7 +1,9 @@
 package alkong_dalkong.backend.Physical.controller;
 
+import alkong_dalkong.backend.Physical.dto.request.PhysicalRequestDto;
 import alkong_dalkong.backend.Physical.dto.response.PhysicalResponseDto;
 import alkong_dalkong.backend.Physical.service.PhysicalService;
+import alkong_dalkong.backend.Physical.service.WeightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,9 @@ public class PhysicalController {
     @Autowired
     private PhysicalService physicalService;
 
+    @Autowired
+    private WeightService weightService;
+
     @GetMapping("/{userId}")
     public ResponseEntity<?> getPhysicalInfo(@PathVariable Long userId, @RequestParam String period) {
         try {
@@ -25,6 +30,18 @@ public class PhysicalController {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(Map.of("code", 400, "error", e.getMessage()));
         } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("code", 500, "error", e.getMessage()));
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addWeight(@RequestBody PhysicalRequestDto requestDto) {
+        try {
+            Long weightId = weightService.addWeight(requestDto);
+            return ResponseEntity.ok().body(Map.of("code", 200, "weightId", weightId));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("code", 400, "error", e.getMessage()));
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("code", 500, "error", e.getMessage()));
         }
     }
