fd31875281285d6f35a29558890d7916dde20b0a
cjh-19 <gns8812@naver.com>
2024-09-09 22:14:52 +0900

    feat: json 파일에서 필요한 데이터를 담고있는 data만 파싱(#66)

---
+++ src/main/java/alkong_dalkong/backend/Physical/util/ApiWeightData.java
@@ -0,0 +1,13 @@
+package alkong_dalkong.backend.Physical.util;
+
+import lombok.Data;
+
+@Data
+public class ApiWeightData {
+
+    private String gender;
+    private int minAge;
+    private int maxAge;
+    private float apiAvgWeight;
+
+}
