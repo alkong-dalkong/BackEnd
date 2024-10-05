2b33879051bf7d39ff396607cfba4e0c5ad5041d
kymin <park48664866@gmail.com>
2024-10-06 00:01:44 +0900

    fix: https적용에 따른 swagger설정 추가

--- src/main/java/alkong_dalkong/backend/Common/config/SwaggerConfig.java
+++ src/main/java/alkong_dalkong/backend/Common/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 
 import org.springdoc.core.customizers.OpenApiCustomizer;
@@ -29,98 +30,104 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
 import alkong_dalkong.backend.Common.Filter.LoginFilter;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
 public class SwaggerConfig {
-    private final ApplicationContext applicationContext;
+        private final ApplicationContext applicationContext;
 
-    @Bean
-    public OpenApiCustomizer customSpringSecurityLoginEndpointCustomiser() {
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER).name("Authorization");
+        @Bean
+        public OpenApiCustomizer customSpringSecurityLoginEndpointCustomiser() {
+                SecurityScheme securityScheme = new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER).name("Authorization");
 
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-        FilterChainProxy filterChainProxy = applicationContext
-                .getBean(AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME, FilterChainProxy.class);
-        return openAPI -> {
-            Components components = openAPI.getComponents();
-            components.addSecuritySchemes("bearerAuth", securityScheme);
-            openAPI.security(Arrays.asList(securityRequirement))
-                    .info(apiInfo());
+                SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+                FilterChainProxy filterChainProxy = applicationContext
+                                .getBean(AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME,
+                                                FilterChainProxy.class);
+                return openAPI -> {
+                        Components components = openAPI.getComponents();
+                        Server server = new Server();
+                        server.setUrl("https://alkongdalkong.duckdns.org");
+                        components.addSecuritySchemes("bearerAuth", securityScheme);
+                        openAPI.security(Arrays.asList(securityRequirement))
+                                        .info(apiInfo()).servers(List.of(server));
 
-            for (SecurityFilterChain filterChain : filterChainProxy.getFilterChains()) {
-                Optional<LoginFilter> optionalLoginFilter = filterChain.getFilters().stream()
-                        .filter(LoginFilter.class::isInstance)
-                        .map(LoginFilter.class::cast)
-                        .findAny();
-                Optional<LoginFilter> optionalLogoutFilter = filterChain.getFilters().stream()
-                        .filter(LoginFilter.class::isInstance)
-                        .map(LoginFilter.class::cast)
-                        .findAny();
-                if (optionalLoginFilter.isPresent()) {
-                    LoginFilter loginFilter = optionalLoginFilter.get();
+                        for (SecurityFilterChain filterChain : filterChainProxy.getFilterChains()) {
+                                Optional<LoginFilter> optionalLoginFilter = filterChain.getFilters().stream()
+                                                .filter(LoginFilter.class::isInstance)
+                                                .map(LoginFilter.class::cast)
+                                                .findAny();
+                                Optional<LoginFilter> optionalLogoutFilter = filterChain.getFilters().stream()
+                                                .filter(LoginFilter.class::isInstance)
+                                                .map(LoginFilter.class::cast)
+                                                .findAny();
+                                if (optionalLoginFilter.isPresent()) {
+                                        LoginFilter loginFilter = optionalLoginFilter.get();
 
-                    Operation operation = new Operation();
+                                        Operation operation = new Operation();
 
-                    Schema<?> schema = new ObjectSchema()
-                            .addProperty("id", new StringSchema())
-                            .addProperty("password", new StringSchema());
+                                        Schema<?> schema = new ObjectSchema()
+                                                        .addProperty("id", new StringSchema())
+                                                        .addProperty("password", new StringSchema());
 
-                    RequestBody requestBody = new RequestBody().content(new Content().addMediaType(
-                            org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-                            new MediaType().schema(schema)));
-                    operation.requestBody(requestBody);
+                                        RequestBody requestBody = new RequestBody().content(new Content().addMediaType(
+                                                        org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+                                                        new MediaType().schema(schema)));
+                                        operation.requestBody(requestBody);
 
-                    ApiResponses apiResponses = new ApiResponses();
-                    apiResponses.addApiResponse(String.valueOf(HttpStatus.OK.value()),
-                            new ApiResponse().description(HttpStatus.OK.getReasonPhrase()));
-                    apiResponses.addApiResponse(String.valueOf(HttpStatus.UNAUTHORIZED.value()),
-                            new ApiResponse().description(HttpStatus.UNAUTHORIZED.getReasonPhrase()));
+                                        ApiResponses apiResponses = new ApiResponses();
+                                        apiResponses.addApiResponse(String.valueOf(HttpStatus.OK.value()),
+                                                        new ApiResponse().description(HttpStatus.OK.getReasonPhrase()));
+                                        apiResponses.addApiResponse(String.valueOf(HttpStatus.UNAUTHORIZED.value()),
+                                                        new ApiResponse().description(
+                                                                        HttpStatus.UNAUTHORIZED.getReasonPhrase()));
 
-                    operation.responses(apiResponses);
-                    operation.addTagsItem("login-endpoint");
-                    PathItem pathItem = new PathItem().post(operation);
-                    openAPI.getPaths().addPathItem("/user/login", pathItem);
-                }
+                                        operation.responses(apiResponses);
+                                        operation.addTagsItem("login-endpoint");
+                                        PathItem pathItem = new PathItem().post(operation);
+                                        openAPI.getPaths().addPathItem("/user/login", pathItem);
+                                }
 
-                if (optionalLogoutFilter.isPresent()) {
-                    LoginFilter logoutFilter = optionalLogoutFilter.get();
+                                if (optionalLogoutFilter.isPresent()) {
+                                        LoginFilter logoutFilter = optionalLogoutFilter.get();
 
-                    Operation operation = new Operation();
+                                        Operation operation = new Operation();
 
-                    ApiResponses apiResponses = new ApiResponses();
-                    apiResponses.addApiResponse(String.valueOf(HttpStatus.OK.value()),
-                            new ApiResponse().description(HttpStatus.OK.getReasonPhrase()));
-                    apiResponses.addApiResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()),
-                            new ApiResponse().description(HttpStatus.BAD_REQUEST.getReasonPhrase()));
-                            
-                    operation.responses(apiResponses);
-                    operation.addTagsItem("logout-endpoint");
-                    PathItem pathItem = new PathItem().post(operation);
-                    openAPI.getPaths().addPathItem("/user/logout", pathItem);
-                }
-            }
-        };
-    }
+                                        ApiResponses apiResponses = new ApiResponses();
+                                        apiResponses.addApiResponse(String.valueOf(HttpStatus.OK.value()),
+                                                        new ApiResponse().description(HttpStatus.OK.getReasonPhrase()));
+                                        apiResponses.addApiResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                                                        new ApiResponse().description(
+                                                                        HttpStatus.BAD_REQUEST.getReasonPhrase()));
 
-    @Bean
-    public GroupedOpenApi openAPI() {
-        return GroupedOpenApi
-        .builder()
-                .group("api")
-                .pathsToMatch("/**")
-                .addOpenApiCustomizer(
-                        customSpringSecurityLoginEndpointCustomiser())
-                .build();
-    }
+                                        operation.responses(apiResponses);
+                                        operation.addTagsItem("logout-endpoint");
+                                        PathItem pathItem = new PathItem().post(operation);
+                                        openAPI.getPaths().addPathItem("/user/logout", pathItem);
+                                }
+                        }
+                };
+        }
 
-    private Info apiInfo() {
-        return new Info()
-                .title("Medical API Documentation")
-                .description("springdoc Swagger UI 테스트")
-                .version("1.0.0");
-    }
+        @Bean
+        public GroupedOpenApi openAPI() {
+                return GroupedOpenApi
+                                .builder()
+                                .group("api")
+                                .pathsToMatch("/**")
+                                .addOpenApiCustomizer(
+                                                customSpringSecurityLoginEndpointCustomiser())
+                                .build();
+        }
+
+        private Info apiInfo() {
+                return new Info()
+                                .title("Medical API Documentation")
+                                .description("springdoc Swagger UI 테스트")
+                                .version("1.0.0");
+        }
 }
