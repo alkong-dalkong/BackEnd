2593049b1a9977117044e7511e0f698312f70e65
kymin <park48664866@gmail.com>
2024-10-14 15:10:01 +0900

    fix: allowedOrigins 변경

--- src/main/java/alkong_dalkong/backend/Common/config/CorsMvcConfig.java
+++ src/main/java/alkong_dalkong/backend/Common/config/CorsMvcConfig.java
@@ -11,8 +11,9 @@ public class CorsMvcConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // 모든 도메인 허용
-        corsConfiguration.addAllowedOriginPattern("https://alkong-dalkong.vercel.app"); // allowCredential과 사용시 쿠키 안 보내짐
+        // 도메인 허용
+        corsConfiguration.addAllowedOrigin("https://alkong-dalkong.vercel.app/");
+        // corsConfiguration.addAllowedOriginPattern("https://alkong-dalkong.vercel.app"); // allowCredential과 사용시 쿠키 안 보내짐
         // 모든 HTTP 메서드 허용
         corsConfiguration.addAllowedMethod("*");
         // 모든 헤더 허용
